b2da8def52c24598284866f51a450fffa40daa15
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-10-29 08:59:43 -0700

    Add detailed balance test, start at autocrine SS. (#11)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,26 @@ using Test
 using Profile
 using TAMode
 
-println("Testing.")
-
 tps = [0.1, 1.0, 10.0, 100.0]
+params = ones(15) * 0.5
 
 
 @testset "Can successfully assemble the parameters." begin
-    TAMode.runTAM(tps, ones(15) * 0.5)
-    @time TAMode.runTAM(tps, ones(15) * 0.5)
+    TAMode.runTAM(tps, params, 1.0)
+    @time TAMode.runTAM(tps, params, 1.0)
 
-    @profile TAMode.runTAM(tps, ones(15) * 0.5)
+    @profile TAMode.runTAM(tps, params, 1.0)
 
-    Profile.print()
+    Profile.print(noisefloor=2.0)
 end
+
+
+@testset "Check for detailed balance at steady-state." begin
+    uLong = TAMode.runTAM([10.0], params, 100.0)
+
+    dnorm = TAMode.TAM_reacti_dnorm(zeros(55), uLong, params, 0.0)
+
+    # TODO: Lower tolerance
+    @test dnorm < 2.0
+end
+
