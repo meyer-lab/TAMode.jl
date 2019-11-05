f81c2ba317dcad72dd05bbd35da2b12993d29606
Micah Bryant <micah.bryant14@gmail.com>
2019-11-05 10:03:27 -0800

    fixed syntax?

--- src/reactCode.jl
+++ src/reactCode.jl
@@ -9,7 +9,7 @@ end
 
 
 mutable struct comprates{T} #do we want this to be T and what does {T} mean
-    rr = Rates() #do i need values for all the rates and does this want to be in the reactCode file or a new one
+    rr::Rates #do i need values for all the rates and does this want to be in the reactCode file or a new one
     fraction::T #Fraction of cell surface covered with PtdSer
     partIn::T #Partitioning rate into PtdSer regions
     gasPart::T #Partitioning of ligand into PtdSer region
