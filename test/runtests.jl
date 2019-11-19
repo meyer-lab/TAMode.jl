ae1395eee5efca1016d86e24f6b78d54c2560187
sumedha-k <sumedha.kanthamneni@gmail.com>
2019-11-18 20:54:32 -0800

    Not done

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,44 @@
 using Test
 using Profile
 using TAMode
-
-println("Testing.")
+using LinearAlgebra
 
 tps = [0.1, 1.0, 10.0, 100.0]
+params = ones(15) * 0.5
 
 
 @testset "Can successfully assemble the parameters." begin
-    rr = TAMode.param(ones(15) * 0.5)
+    TAMode.runTAM(tps, params, 1.0)
+    @time TAMode.runTAM(tps, params, 1.0)
 
-    TAMode.runTAM(tps, ones(15) * 0.5)
-    @time TAMode.runTAM(tps, ones(15) * 0.5)
+    @profile TAMode.runTAM(tps, params, 1.0)
 
-    @profile TAMode.runTAM(tps, ones(15) * 0.5)
-
-    Profile.print()
+    Profile.print(noisefloor=2.0)
 end
+
+
+@testset "Check for detailed balance at steady-state." begin
+    uLong = TAMode.runTAM([1000.0], params, 100.0)
+
+    dnorm = TAMode.TAM_reacti_dnorm(zeros(55), uLong, params, 0.0)
+
+    # TODO: Lower tolerance
+    @test dnorm < 2.0
+end
+
+@testset "Swapping Ig1 with Ig2 doesn't change anything." begin
+    rr = TAMode.param(params)
+    rrSwap = TAMode.swapIgs(rr)
+
+    data = TAMode.runTAM(tps, rr, 10.0)
+    dataSwap = TAMode.runTAM(tps, rrSwap, 10.0)
+
+    # Test that the phosphorylated receptor matches up
+    @test all(data * TAMode.pY .≈ dataSwap * TAMode.pY)
+    @test all(data * TAMode.total .≈ dataSwap * TAMode.total)
+    @test all(data * TAMode.surface .≈ dataSwap * TAMode.surface)
+    @test all(data * TAMode.boundLig .≈ dataSwap * TAMode.boundLig)
+end
+
+
+
