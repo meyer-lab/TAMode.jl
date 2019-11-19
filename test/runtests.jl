7298145eb7208f2b123ad019ad475b7c27d4ca17
sumedha-k <sumedha.kanthamneni@gmail.com>
2019-11-18 20:54:32 -0800

    Not done

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,11 @@ using Profile
 using TAMode
 using LinearAlgebra
 
+<<<<<<< HEAD
 tps = [0.1, 1.0, 10.0, 100.0, 1000.0]
+=======
+tps = [0.1, 1.0, 10.0, 100.0]
+>>>>>>> Not done
 params = ones(15) * 0.5
 
 
@@ -12,6 +16,7 @@ params = ones(15) * 0.5
     @time TAMode.runTAM(tps, params, 1.0)
 
     @profile TAMode.runTAM(tps, params, 1.0)
+<<<<<<< HEAD
     @profile TAMode.runTAM(tps, params, 10.0)
     @profile TAMode.runTAM(tps, params, 100.0)
     @profile TAMode.runTAM(tps, params, 1000.0)
@@ -33,10 +38,15 @@ end
     secondV = TAMode.runTAMinit([1000000.0], tt, firstV)
 
     @test dot(firstV, TAMode.total) - dot(secondV, TAMode.total) < 0.0001
+=======
+
+    Profile.print(noisefloor=2.0)
+>>>>>>> Not done
 end
 
 
 @testset "Check for detailed balance at steady-state." begin
+<<<<<<< HEAD
     rr = TAMode.param(params)
 
     rr.TAMs[1].expression = 0.0
@@ -80,3 +90,29 @@ end
     TAMode.TAM_reacti(dnorm, uLong, params, 0.0)
     @test all(dnorm .< 0.05)
 end
+=======
+    uLong = TAMode.runTAM([1000.0], params, 100.0)
+
+    dnorm = TAMode.TAM_reacti_dnorm(zeros(55), uLong, params, 0.0)
+
+    # TODO: Lower tolerance
+    @test dnorm < 2.0
+end
+
+@testset "Swapping Ig1 with Ig2 doesn't change anything." begin
+    rr = TAMode.param(params)
+    rrSwap = TAMode.swapIgs(rr)
+
+    data = TAMode.runTAM(tps, rr, 10.0)
+    dataSwap = TAMode.runTAM(tps, rrSwap, 10.0)
+
+    # Test that the phosphorylated receptor matches up
+    @test all(data * TAMode.pY .≈ dataSwap * TAMode.pY)
+    @test all(data * TAMode.total .≈ dataSwap * TAMode.total)
+    @test all(data * TAMode.surface .≈ dataSwap * TAMode.surface)
+    @test all(data * TAMode.boundLig .≈ dataSwap * TAMode.boundLig)
+end
+
+
+
+>>>>>>> Not done
