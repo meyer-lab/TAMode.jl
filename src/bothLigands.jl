ad8f311a613057ce6ddae5c3ccae9ad010a36c5b
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-12-09 20:35:54 -0800

    Add two ligand reaction function (#42)

--- src/bothLigands.jl
+++ src/bothLigands.jl
@@ -20,3 +20,59 @@ mutable struct Lsrates{T}
 	curL::MVector{2, T}
 	xFwd::T
 end
+
+
+function react_module(R, dR, curL, r)
+	dRr = SVector{30}([r.GBinding[1]*curL[1]*R[1] - r.GBinding[2]*R[2],
+					   r.GBinding[3]*curL[1]*R[1] - r.GBinding[4]*R[3],
+					   r.PBinding[1]*curL[2]*R[1] - r.PBinding[2]*R[4],
+					   r.PBinding[3]*curL[2]*R[1] - r.PBinding[4]*R[5],
+					   r.GBinding[3]*curL[1]*R[2] - r.GBinding[4]*R[6],
+					   r.GBinding[1]*curL[1]*R[3] - r.GBinding[2]*R[6],
+					   r.PBinding[1]*curL[2]*R[3] - r.PBinding[2]*R[7],
+					   r.GBinding[3]*curL[1]*R[4] - r.GBinding[4]*R[7],
+					   r.PBinding[3]*curL[2]*R[4] - r.PBinding[4]*R[8],
+					   r.PBinding[1]*curL[2]*R[5] - r.PBinding[2]*R[8],
+					   r.GBinding[1]*curL[1]*R[5] - r.GBinding[2]*R[9],
+					   r.PBinding[3]*curL[2]*R[2] - r.PBinding[4]*R[9],
+
+					   r.xFwd*R[2]*R[2] - r.xRev[1]*R[10],
+					   r.xFwd*R[1]*R[6] - r.xRev[2]*R[10],
+					   r.xFwd*R[3]*R[3] - r.xRev[3]*R[10],
+					   r.xFwd*R[2]*R[1] - r.xRev[4]*R[11],
+					   r.xFwd*R[3]*R[1] - r.xRev[5]*R[11],
+					   r.xFwd*R[1]*R[5] - r.xRev[6]*R[12],
+					   r.xFwd*R[1]*R[4] - r.xRev[7]*R[12],
+					   r.xFwd*R[4]*R[4] - r.xRev[8]*R[13],
+					   r.xFwd*R[1]*R[8] - r.xRev[9]*R[13],
+					   r.xFwd*R[5]*R[5] - r.xRev[10]*R[13],
+					   r.xFwd*R[7]*R[1] - r.xRev[11]*R[14],
+					   r.xFwd*R[9]*R[1] - r.xRev[12]*R[14],
+					   r.xFwd*R[2]*R[4] - r.xRev[13]*R[14],
+					   r.xFwd*R[3]*R[5] - r.xRev[14]*R[14],
+					   r.xFwd27*R[12]*curL[2] - r.xRev[15]*R[13],
+					   r.xFwd27*R[11]*curL[2] - r.xRev[15]*R[14],
+					   r.xFwd29*R[12]*curL[1] - r.xRev[16]*R[14],
+					   r.xFwd29*R[11]*curL[1] - r.xRev[16]*R[10]])
+
+	dR[1] = -sum(dRr[SVector(1, 2, 3, 4, 14, 16, 17, 18, 19, 21, 23, 24)]) # A
+	dR[1] = dRr[1] - sum(dRr[SVector(5, 12, 13, 13, 16, 25)]) # AG (first site)
+	dR[2] = dRr[2] - sum(dRr[SVector(6, 7, 15, 15, 17, 26)]) # AG (second site)
+	dR[3] = dRr[3] - sum(dRr[SVector(8, 9, 19, 20, 20, 25)]) # AP (first site)
+	dR[4] = dRr[4] - sum(dRr[SVector(10, 11, 18, 22, 22, 26)]) # AP (second site)
+	dR[5] = dRr[5] + dRr[6] - dRr[14] # AGG
+	dR[6] = dRr[7] + dRr[8] - dRr[23] # APG
+	dR[7] = dRr[9] + dRr[10] - dRr[21] # APP
+	dR[8] = dRr[11] + dRr[12] - dRr[24] # AGP
+
+	dR[9] = sum(dRr[SVector(13, 14, 15, 30)]) # AGGA
+	dR[10] = dRr[16] + dRr[17] - dRr[28] - dRr[30] # AGA
+	dR[11] = dRr[18] + dRr[19] - dRr[27] - dRr[29] # APA
+	dR[12] = sum(dRr[SVector(20, 21, 22, 27)]) # APPA
+	dR[13] = sum(dRr[SVector(23, 24, 25, 26, 28, 29)]) # APGA
+
+	dR[14] = -sum(dRr[SVector(1, 2, 5, 6, 8, 11, 29, 30)]) - r.kDeg*R[15] # Gasi
+	dR[15] = -sum(dRr[SVector(3, 4, 7, 9, 10, 12, 27, 28)]) - r.kDeg*R[16] # PROSi
+
+	return norm(dRr)
+end
--- src/compModel.jl
+++ src/compModel.jl
@@ -1,7 +1,7 @@
 
 mutable struct comprates{T} #do we want this to be T and what does {T} mean
-    rr::Rates #do i need values for all the rates and does this want to be in the reactCode file or a new one
-    fraction::T #Fraction of cell surface covered with PtdSer
-    partIn::T #Partitioning rate into PtdSer regions
-    gasPart::T #Partitioning of ligand into PtdSer region
+	rr::Rates #do i need values for all the rates and does this want to be in the reactCode file or a new one
+	fraction::T #Fraction of cell surface covered with PtdSer
+	partIn::T #Partitioning rate into PtdSer regions
+	gasPart::T #Partitioning of ligand into PtdSer region
 end
