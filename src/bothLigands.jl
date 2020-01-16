f2887e0e287ece0dc382a3f846ae50a992d5098b
Amanda <ajtsao1@gmail.com>
2020-01-16 11:53:04 -0800

    bothLigands
    TAM_react
    I'm not completely sure if this is the correct use of a view.

--- src/bothLigands.jl
+++ src/bothLigands.jl
@@ -20,3 +20,28 @@ mutable struct Lsrates{T}
 	curL::MVector{2, T}
 	xFwd::T
 end
+
+# @brief Callback function from CVode to handle ODE solving
+# @param[in] R  Double vector for current state of model
+# @param dR  Double vector for returning derivative of current state
+# @return Always returns 0 for success
+
+
+function TAM_react(const R::T, const dR::T)
+    tr = view(pparams,:,:)
+    
+    react_module(R, dR, tr.curL) # We give the same internal ligand address as below because it will be overwritten
+    react_module(R+iR, dR+iR, {{R[15+iR]/tr.internalV, R[16+iR]/tr.internalV}})
+
+    dR[1] += tr.expression
+
+    for UInt i = 1:8
+        trafFunc(@view dR[i], @view dR[i+iR], tr.internalize, R[i], R[i+iR], tr.kRec, tr.kDeg, tr.fElse, tr.internalFrac)
+    end
+    
+    for UInt i = 9:13
+        trafFunc(@view dR[i], @view dR[i+iR], tr.pYinternalize, R[i], R[i+iR], tr.kRec, tr.kDeg, tr.fElse, tr.internalFrac)
+    end
+        
+    return 0
+end
