11276f52d77a219ecaa896a9e023d570a42034cd
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-01-16 08:32:16 -0800

    Update compModel.jl

--- src/compModel.jl
+++ src/compModel.jl
@@ -6,8 +6,8 @@ mutable struct comprates{T} #do we want this to be T and what does {T} mean
     gasPart::T #Partitioning of ligand into PtdSer region
 end
 
-function compParamm(compIn::Vector{T})
-    ppparams = comprates{T}
+function compParamm(compIn::Vector)
+    ppparams = comprates{eltype(compIn)}
     ppparams.rr = param(compIn[4:end])
     ppparams.fraction = compIn[1]
     ppparams.partIn = compIn[2]
