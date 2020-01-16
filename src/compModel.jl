d8acb0af521dced314834388f41e4e5ffe972412
sumedha-k <sumedha.kanthamneni@gmail.com>
2020-01-15 20:11:23 -0800

    compParam

--- src/compModel.jl
+++ src/compModel.jl
@@ -5,3 +5,19 @@ mutable struct comprates{T} #do we want this to be T and what does {T} mean
     partIn::T #Partitioning rate into PtdSer regions
     gasPart::T #Partitioning of ligand into PtdSer region
 end
+
+function compParamm(compIn::compT)
+    paramsIn::trafT
+    size = size(paramsIn)[1]
+    for i in 1:size
+        compIn[i+3] = paramsIn[i]
+    end
+    
+    ppparams::comprates
+    ppparams.rr = TAM.Param(paramsIn)
+    ppparams.fraction = compIn[1]
+    ppparams.partIn = compIn[2]
+    ppparams.gasPart = compIn[3]
+    
+    return ppparams
+end
