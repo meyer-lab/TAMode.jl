f027e1ab03b5d9ced83d50b33c1bb1de982aa4a8
Sumedha Kanthamneni <sumedha.kanthamneni@gmail.com>
2020-01-18 09:53:20 -0800

    compParam (#53)
    * Not done
    
    * test mass conservation
    
    * fixed merge conflict
    
    * compParam
    
    * compParam
    
    * compParam
    
    * Update compModel.jl
    
    * Update compModel.jl
    
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- src/compModel.jl
+++ src/compModel.jl
@@ -5,3 +5,13 @@ mutable struct comprates{T} #do we want this to be T and what does {T} mean
     partIn::T #Partitioning rate into PtdSer regions
     gasPart::T #Partitioning of ligand into PtdSer region
 end
+
+function compParamm(compIn::Vector)
+    ppparams = comprates{eltype(compIn)}
+    ppparams.rr = param(compIn[4:end])
+    ppparams.fraction = compIn[1]
+    ppparams.partIn = compIn[2]
+    ppparams.gasPart = compIn[3]
+    
+    return ppparams
+end
