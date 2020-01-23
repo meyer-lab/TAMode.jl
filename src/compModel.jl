44d244ed23199c9bd3f0b17ca10b55688a689047
Rayo Suseno <rsuseno@g.ucla.edu>
2020-01-22 19:20:55 -0800

    calc Stim

--- src/compModel.jl
+++ src/compModel.jl
@@ -6,12 +6,54 @@ mutable struct comprates{T} #do we want this to be T and what does {T} mean
     gasPart::T #Partitioning of ligand into PtdSer region
 end
 
-function compParamm(compIn::Vector)
-    ppparams = comprates{eltype(compIn)}
-    ppparams.rr = param(compIn[4:end])
-    ppparams.fraction = compIn[1]
-    ppparams.partIn = compIn[2]
-    ppparams.gasPart = compIn[3]
+function calcStim(tps::Array{Float64,1}, params, gasStim::Float64)::Array{Float64,2}
+    @assert all(tps .>= 0.0)
 
-    return ppparams
+    solInit = getAutocrine(params, TAM_reacti, 55)
+
+    if params isa Rates
+        params.gasCur = gasStim
+    else
+        params[7] = gasStim
+    end
+
+    prob = ODEProblem(TAM_reacti, solInit, maximum(tps), params)
+
+    sol = solve(prob, AutoTsit5(TRBDF2(linsolve=LinSolveGMRES())); isoutofdomain=domainDef)
+    solut = sol(tps).u
+
+    if length(tps) > 1
+        solut = vcat(transpose.(solut)...)
+    else
+        solut = reshape(solut[1], (1, length(solInit)))
+    end
+
+    return solut
 end
+
+
+function calcStimPtdser(tps::Array{Float64,1}, params, gasStim::Float64)::Array{Float64,2}
+    @assert all(tps .>= 0.0)
+
+    solInit = getAutocrine(params, TAM_reacti, 55)
+
+    if params isa Rates
+        params.gasCur = gasStim
+    else
+        params[7] = gasStim
+    end
+
+    prob = ODEProblem(TAM_reacti, solInit, maximum(tps), params)
+
+    sol = solve(prob, AutoTsit5(TRBDF2(linsolve=LinSolveGMRES())); isoutofdomain=domainDef)
+    solut = sol(tps).u
+
+    if length(tps) > 1
+        solut = vcat(transpose.(solut)...)
+    else
+        solut = reshape(solut[1], (1, length(solInit)))
+    end
+
+    return solut
+end
+
