2fdf763a0478fa1a68fb8e723faa7fc1261ef7a7
Aaron Meyer <ameyer@ucla.edu>
2020-01-30 08:06:32 -0800

    Working.

--- src/BLI.jl
+++ src/BLI.jl
@@ -20,40 +20,36 @@ function importData(cond)
     conc = df[1,2:end]
     tps = df[5:end,1]
     measVal = df[5:end,2:end]
-    return parse.(Float64,Array(conc)), parse.(Float64, tps), parse.(Float64,Matrix(measVal))
+    return parse.(Float64, Array(conc)), parse.(Float64, tps), parse.(Float64, Matrix(measVal))
 end
 
 
-@model BLI(tps,conc,bindData) = begin
+@model BLI(tps, conc, bindData) = begin
     Tshift = 822.2
     Kon ~ Normal(6., 0.5)
     Kdis ~ Normal(2., 1.)
     Tshift = tps[1] + 599.9
-    theor_save = 0
-    exp_save = 0
+    theor_save = []
+    exp_save = []
+
     for i in 1:length(conc)
         tBind = tps[tps.<Tshift].-tps[1]
-        L0 = conc[i]
-        exp_bind = bindData[:,i]
             
-        bind_step = R1Calc(L0, Kon, Kdis, tBind)
+        bind_step = R1Calc(conc[i], Kon, Kdis, tBind)
         unbind_step = R2Calc(bind_step[end], Kdis, tps[tps .> Tshift] .- Tshift)
         theor_bind = vcat(bind_step[:], unbind_step)
+
         if i == 1
             theor_save = theor_bind
-            exp_save = exp_bind
+            exp_save = bindData[:,i]
         else
             theor_save = vcat(theor_save, theor_bind)
-            exp_save = vcat(exp_save, exp_bind)
+            exp_save = vcat(exp_save, bindData[:,i])
         end
     end
 
     Rmax = mean(theor_save)/mean(exp_save)
-
     residuals = exp_save .- (theor_save*Rmax)
-    N = length(residuals)
-    #resMean = mean(residuals)
-    #resStd = stdm(residuals, resMean)
-    #residual ~ Normal(resMean, abs(resStd))
-    residuals ~ MvNormal(zeros(N), 2*ones(N))
+
+    residuals ~ MvNormal(zeros(length(residuals)), ones(length(residuals)) * std(residuals))
 end
