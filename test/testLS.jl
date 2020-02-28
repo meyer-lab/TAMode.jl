eebbf60c3a39bdc205abb9437bdfa28545715799
Aaron Meyer <a@ameyer.me>
2020-02-28 11:01:01 -0800

    Edits.

--- test/testLS.jl
+++ test/testLS.jl
@@ -1,17 +1,17 @@
 
 @testset "Test if bothLigands function works." begin
     pp = TAMode.Lsparam(fill(0.2, 9))
-    ss = TAMode.getAutocrineLS(pp)
+    ss = TAMode.getAutocrine(params, TAMreactLS, 30)
 end
 
 
 @testset "Make sure code LS upholds mass conservation." begin
     tt = TAMode.Lsparam(fill(0.2, 9))
-    firstV = TAMode.getAutocrineLS(tt)
+    firstV = TAMode.getAutocrine(params, TAMreactLS, 30)
 
     tt.kDeg = 0
     tt.expression = 0
-    tt.curL = (100, 1000)
+    tt.curL = (100, 100)
 
     secondV = TAMode.runTAMinitLS([1000000.0], tt, firstV)
 
