84283dcde530506fc90f4c9a7fc93bd532b7d1a4
Micah Bryant <39173191+micah-bryant@users.noreply.github.com>
2020-02-28 08:02:17 -0800

    BLI plotting and a fix (#92)
    * initial plotting function
    
    * added overlaid graphs and subtle changes to BLI model
    
    * 1 iteration of the 25
    
    * fixed R1calc and added working plotting function
    
    * Remove pictures.
    
    * Fix.
    
    * Remove more.
    
    * Consolidate.
    
    * Clean BLI
    
    * Fix.
    
    * Move plots notebook.
    
    * Fix BLI
    
    * Add
    
    * Reduce notebook.
    
    * Simplify
    
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- src/BLI.jl
+++ src/BLI.jl
@@ -5,7 +5,7 @@ gas6_TFL = "../data/TFL-010820.csv"
 
 """ Calculation for binding step. """
 function R1Calc(conc::Real, Kon::Real, Kdis::Real, tps)
-    return conc / (Kdis / Kon + conc) * (1 .- (1 / (exp.((Kon * conc + Kdis) * tps))))
+    return conc / (Kdis / Kon + conc) * (1 .- (1 ./ (exp.((Kon .* conc .+ Kdis) * tps))))
 end
 
 
@@ -24,26 +24,33 @@ function importData(cond)
 end
 
 
-@model BLI(tps, conc, bindData) = begin
-    Tshift = 822.2
-    Kon ~ LogNormal(6.0, 0.5)
-    Kdis ~ LogNormal(1.0, 1.0)
-    Rmax ~ LogNormal(-1.0, 0.1)
+function bindingCalc(tps::Vector, Kon::Real, Kdis::Real, Rmax::Real)
     Tshift = tps[1] + 599.9
     tBind = tps[tps .< Tshift] .- tps[1]
     tUnbind = tps[tps .> Tshift] .- Tshift
 
+    bind_step = R1Calc(conc[i], Kon, Kdis, tBind)
+    unbind_step = R2Calc(bind_step[end], Kdis, tUnbind)
+    theor_bind = vcat(bind_step[:], unbind_step) * Rmax
+
+    return theor_bind
+end
+
+
+@model BLI(tps, conc, bindData) = begin
+    Kon ~ LogNormal(6.0, 0.5)
+    Kdis ~ LogNormal(1.0, 1.0)
+    Rmax ~ LogNormal(-1.0, 0.1)
+
     resid_save = []
 
-    for i = 1:length(conc)
-        bind_step = R1Calc(conc[i], Kon, Kdis, tBind)
-        unbind_step = R2Calc(bind_step[end], Kdis, tUnbind)
-        theor_bind = vcat(bind_step[:], unbind_step)
+    for i = 1:(length(conc) - 1)
+        theor_bind = bindingCalc(tps, Kon, Kdis, Rmax)
 
         if i == 1
-            resid_save = bindData[:, i] .- theor_bind * Rmax
+            resid_save = bindData[:, i] .- theor_bind
         else
-            resid_save = vcat(resid_save, bindData[:, i] .- theor_bind * Rmax)
+            resid_save = vcat(resid_save, bindData[:, i] .- theor_bind)
         end
     end
 
