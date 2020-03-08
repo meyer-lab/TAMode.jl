babc9ce37ef463da7fea95472acf95ecc58ff032
Micah Bryant <micah.bryant14@gmail.com>
2020-03-08 12:38:09 -0700

    fixed some code/weird sampling results

--- src/BLI.jl
+++ src/BLI.jl
@@ -5,13 +5,13 @@ gas6_TFL = "../data/TFL-010820.csv"
 
 """ Calculation for binding step. """
 function R1Calc(conc::Real, Kon::Real, Kdis::Real, tps)
-    return conc / (Kdis / Kon + conc) * (1 .- (1 ./ (exp.((Kon .* conc .+ Kdis) * tps))))
+    return conc ./ (Kdis ./ Kon + conc) .* (1 .- (1 ./ (exp.((Kon .* conc .+ Kdis) .* tps))))
 end
 
 
 """ Calculation for unbinding step. """
 function R2Calc(R::Real, Kdis::Real, tps)
-    return R * exp.(-Kdis * tps)
+    return R .* exp.(-Kdis .* tps)
 end
 
 
@@ -24,12 +24,12 @@ function importData(cond)
 end
 
 
-function bindingCalc(tps::Vector, Kon::Real, Kdis::Real, Rmax::Real)
+function bindingCalc(tps::Vector, Kon::Real, Kdis::Real, Rmax::Real, conc::Real)
     Tshift = tps[1] + 599.9
     tBind = tps[tps .< Tshift] .- tps[1]
     tUnbind = tps[tps .> Tshift] .- Tshift
 
-    bind_step = R1Calc(conc[i], Kon, Kdis, tBind)
+    bind_step = R1Calc(conc, Kon, Kdis, tBind)
     unbind_step = R2Calc(bind_step[end], Kdis, tUnbind)
     theor_bind = vcat(bind_step[:], unbind_step) * Rmax
 
@@ -45,7 +45,7 @@ end
     resid_save = []
 
     for i = 1:(length(conc) - 1)
-        theor_bind = bindingCalc(tps, Kon, Kdis, Rmax)
+        theor_bind = bindingCalc(tps, Kon, Kdis, Rmax, conc[i])
 
         if i == 1
             resid_save = bindData[:, i] .- theor_bind
