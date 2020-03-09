0ea3778d5f1939ff2c40e301f77dc4d61d0419ee
Rayo Suseno <rsuseno@g.ucla.edu>
2020-03-08 21:41:51 -0700

    Add 2 tests to testLS.jl

--- test/testLS.jl
+++ test/testLS.jl
@@ -17,3 +17,26 @@ end
 
     @test dot(firstV, TAMode.totalLS) - dot(secondV, TAMode.totalLS) < 0.0001
 end
+
+@testset "Ensure that system reaches equilibrium(LS)." begin
+    pp = TAMode.Lsparam(LSparams)
+    uLong = TAMode.getAutocrine(pp, TAMode.TAMreactLS, 30)
+    dnorm = zeros(30)
+    TAMode.TAMreactLS(dnorm, uLong, pp, 0.0)
+    @test all(dnorm .< 0.05)
+end
+
+@testset "Make sure that TAM surface don't explode at long time in reaction code (LS)." begin
+    tt = TAMode.Lsparam(LSparams)
+    firstSurf = TAMode.getAutocrine(tt, TAMode.TAMreactLS, 30)
+
+    tt.expression = 0.0
+    tt.kRec = 0.0
+    tt.internalize = 0.0
+    tt.pYinternalize = 0.0
+    tt.curL = (10, 10)
+
+    secondSurf = TAMode.runTAMinit([100.0], tt, TAMode.TAMreactLS, firstSurf)
+
+    @test_broken isapprox(dot(firstSurf, TAMode.surface), dot(secondSurf, TAMode.surface), rtol = 1.0e-5)
+end
