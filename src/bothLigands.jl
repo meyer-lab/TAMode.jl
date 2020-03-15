abd63e5fe4afd7ac533d34a7beadd729275b78b1
Rayo Suseno <53069907+benediktusrayosuseno@users.noreply.github.com>
2020-03-15 16:20:35 -0700

    Add 2 tests to testLS.jl (#114)
    * Add 2 tests to testLS.jl
    
    * Fix.
    
    * Linting.
    
    * Fix.
    
    * Try this.
    
    * Update testLS.jl
    
    Co-authored-by: Rayo Suseno <rsuseno@g.ucla.edu>
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>
    Co-authored-by: Aaron Meyer <aaron@asmlab.org>

--- src/bothLigands.jl
+++ src/bothLigands.jl
@@ -119,14 +119,17 @@ end
 
 
 totalLS = vcat(ones(9), 2 * ones(5), ones(9) * internalFrac, 2 * ones(5) * internalFrac, zeros(2))
+surfaceLS = vcat(ones(14), zeros(16))
 
 
 function TAMreactLS(dR, R, tr, t)
-    react_module(view(R, 1:14), view(dR, 1:14), tr.curL, tr)
-    react_module(view(R, 15:28), view(dR, 15:30), view(R, 29:30) / internalV, tr)
+    dnorm = react_module(view(R, 1:14), view(dR, 1:14), tr.curL, tr)
+    dnorm += react_module(view(R, 15:28), view(dR, 15:30), view(R, 29:30) / internalV, tr)
 
     dR[1] += tr.expression
 
     trafFunc(view(dR, 1:9), view(dR, 15:23), tr.internalize, R[1:9], R[15:23], tr.kRec, tr.kDeg, tr.fElse)
     trafFunc(view(dR, 10:14), view(dR, 24:28), tr.pYinternalize, R[10:14], R[24:28], tr.kRec, tr.kDeg, tr.fElse)
+
+    return dnorm
 end
