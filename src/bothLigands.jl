7f73fbc4e340c0f2f371a3c2f4c52506268ba54b
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-03-15 08:36:09 -0700

    Fix LS model (#122)
    * Clean
    
    * Edits.
    
    Co-authored-by: Aaron Meyer <aaron@asmlab.org>

--- src/bothLigands.jl
+++ src/bothLigands.jl
@@ -1,5 +1,4 @@
 
-
 mutable struct Lsrates{T}
     GBinding::MVector{4, T} # Gas6 fwd/rev rate for Ig1, then Ig2
     PBinding::MVector{4, T} # ProS fwd/rev rate for Ig1, then Ig2
@@ -27,14 +26,9 @@ function Lsparam(params::Vector)
     out = Lsrates{eltype(params)}(
         (fwdDef, params[5], fwdDef, params[6]),
         (fwdDef, params[7], fwdDef, params[8]),
-        zeros(16),
-        0.0,
-        0.0,
-        5.8e-2, # kRec
-        2.2e-3, #kDeg
-        0.2, # fElse
-        0.03, # internalize
-        0.3, # pYinternalize
+        zeros(16), 0.0, 0.0,
+        5.8e-2, 2.2e-3, # kRec, kDeg
+        0.2, 0.03, 0.3, # fElse, internalize, pYinternalize
         params[4], # expression
         params[2:3],
         (0.0, 0.0),
@@ -100,34 +94,36 @@ function react_module(R, dR, curL, r)
     ])
 
     dR[1] = -sum(dRr[SVector(1, 2, 3, 4, 14, 16, 17, 18, 19, 21, 23, 24)]) # A
-    dR[1] = dRr[1] - sum(dRr[SVector(5, 12, 13, 13, 16, 25)]) # AG (first site)
-    dR[2] = dRr[2] - sum(dRr[SVector(6, 7, 15, 15, 17, 26)]) # AG (second site)
-    dR[3] = dRr[3] - sum(dRr[SVector(8, 9, 19, 20, 20, 25)]) # AP (first site)
-    dR[4] = dRr[4] - sum(dRr[SVector(10, 11, 18, 22, 22, 26)]) # AP (second site)
-    dR[5] = dRr[5] + dRr[6] - dRr[14] # AGG
-    dR[6] = dRr[7] + dRr[8] - dRr[23] # APG
-    dR[7] = dRr[9] + dRr[10] - dRr[21] # APP
-    dR[8] = dRr[11] + dRr[12] - dRr[24] # AGP
+    dR[2] = dRr[1] - sum(dRr[SVector(5, 12, 13, 13, 16, 25)]) # AG (first site)
+    dR[3] = dRr[2] - sum(dRr[SVector(6, 7, 15, 15, 17, 26)]) # AG (second site)
+    dR[4] = dRr[3] - sum(dRr[SVector(8, 9, 19, 20, 20, 25)]) # AP (first site)
+    dR[5] = dRr[4] - sum(dRr[SVector(10, 11, 18, 22, 22, 26)]) # AP (second site)
+    dR[6] = dRr[5] + dRr[6] - dRr[14] # AGG
+    dR[7] = dRr[7] + dRr[8] - dRr[23] # APG
+    dR[8] = dRr[9] + dRr[10] - dRr[21] # APP
+    dR[9] = dRr[11] + dRr[12] - dRr[24] # AGP
 
-    dR[9] = sum(dRr[SVector(13, 14, 15, 30)]) # AGGA
-    dR[10] = dRr[16] + dRr[17] - dRr[28] - dRr[30] # AGA
-    dR[11] = dRr[18] + dRr[19] - dRr[27] - dRr[29] # APA
-    dR[12] = sum(dRr[SVector(20, 21, 22, 27)]) # APPA
-    dR[13] = sum(dRr[SVector(23, 24, 25, 26, 28, 29)]) # APGA
+    dR[10] = sum(dRr[SVector(13, 14, 15, 30)]) # AGGA
+    dR[11] = dRr[16] + dRr[17] - dRr[28] - dRr[30] # AGA
+    dR[12] = dRr[18] + dRr[19] - dRr[27] - dRr[29] # APA
+    dR[13] = sum(dRr[SVector(20, 21, 22, 27)]) # APPA
+    dR[14] = sum(dRr[SVector(23, 24, 25, 26, 28, 29)]) # APGA
 
-    dR[14] = -sum(dRr[SVector(1, 2, 5, 6, 8, 11, 29, 30)]) - r.kDeg * R[15] # Gasi
-    dR[15] = -sum(dRr[SVector(3, 4, 7, 9, 10, 12, 27, 28)]) - r.kDeg * R[16] # PROSi
+    if length(dR) == 16
+        dR[15] = -sum(dRr[SVector(1, 2, 5, 6, 8, 11, 29, 30)]) - r.kDeg * curL[1] # Gasi
+        dR[16] = -sum(dRr[SVector(3, 4, 7, 9, 10, 12, 27, 28)]) - r.kDeg * curL[2] # PROSi
+    end
 
     return norm(dRr)
 end
 
 
-totalLS = vcat(ones(8), 2 * ones(5), ones(8) * internalFrac, 2 * ones(5) * internalFrac, zeros(4))
+totalLS = vcat(ones(9), 2 * ones(5), ones(9) * internalFrac, 2 * ones(5) * internalFrac, zeros(2))
 
 
 function TAMreactLS(dR, R, tr, t)
-    react_module(R, dR, tr.curL, tr)
-    react_module(view(R, 14:30), view(dR, 14:30), view(R, 29:30) / internalV, tr)
+    react_module(view(R, 1:14), view(dR, 1:14), tr.curL, tr)
+    react_module(view(R, 15:28), view(dR, 15:30), view(R, 29:30) / internalV, tr)
 
     dR[1] += tr.expression
 
