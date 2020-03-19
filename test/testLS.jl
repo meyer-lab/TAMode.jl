c124b1c3e3bb0a93558648366b5347c8d0ce74ab
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-03-19 15:07:44 -0700

    Performance (#131)
    * Update manifest.
    
    * Edits.
    
    * Update testLS.jl

--- test/testLS.jl
+++ test/testLS.jl
@@ -1,45 +1,45 @@
+@testset "Two ligand model." begin
+    @testset "LS: Test if bothLigands function works." begin
+        pp = TAMode.Lsparam(fill(0.2, 9))
+        ss = TAMode.getAutocrine(pp, TAMode.TAMreactLS, 30)
+    end
 
-@testset "LS: Test if bothLigands function works." begin
-    pp = TAMode.Lsparam(fill(0.2, 9))
-    ss = TAMode.getAutocrine(pp, TAMode.TAMreactLS, 30)
-end
+    @testset "LS: Make sure code upholds mass conservation." begin
+        tt = TAMode.Lsparam(fill(0.2, 9))
+        firstV = TAMode.getAutocrine(tt, TAMode.TAMreactLS, 30)
 
+        tt.kDeg = 0
+        tt.expression = 0
+        tt.curL = (100, 100)
 
-@testset "LS: Make sure code upholds mass conservation." begin
-    tt = TAMode.Lsparam(fill(0.2, 9))
-    firstV = TAMode.getAutocrine(tt, TAMode.TAMreactLS, 30)
+        secondV = TAMode.runTAMinit([1.0e6], tt, TAMode.TAMreactLS, firstV)
 
-    tt.kDeg = 0
-    tt.expression = 0
-    tt.curL = (100, 100)
+        @test dot(firstV, TAMode.totalLS) - dot(secondV, TAMode.totalLS) < 0.0001
+    end
 
-    secondV = TAMode.runTAMinit([1.0e6], tt, TAMode.TAMreactLS, firstV)
+    @testset "LS: Ensure that system reaches detailed balance." begin
+        pp = TAMode.Lsparam(fill(0.2, 9))
 
-    @test dot(firstV, TAMode.totalLS) - dot(secondV, TAMode.totalLS) < 0.0001
-end
+        uLong = TAMode.getAutocrine(pp, TAMode.TAMreactLS, 30)
 
-@testset "LS: Ensure that system reaches detailed balance." begin
-    pp = TAMode.Lsparam(fill(0.2, 9))
+        dnorm = TAMode.TAMreactLS(ones(30), uLong, pp, 0.0)
 
-    uLong = TAMode.getAutocrine(pp, TAMode.TAMreactLS, 30)
+        @test dnorm < 0.05
+    end
 
-    dnorm = TAMode.TAMreactLS(ones(30), uLong, pp, 0.0)
+    @testset "LS: Make sure that surface total is preserved without trafficking." begin
+        tt = TAMode.Lsparam(fill(0.2, 9))
+        firstSurf = TAMode.getAutocrine(tt, TAMode.TAMreactLS, 30)
 
-    @test dnorm < 0.05
-end
+        tt.expression = 0.0
+        tt.kRec = 0.0
+        tt.internalize = 0.0
+        tt.pYinternalize = 0.0
+        tt.curL = (10, 10)
 
-@testset "LS: Make sure that surface total is preserved without trafficking." begin
-    tt = TAMode.Lsparam(fill(0.2, 9))
-    firstSurf = TAMode.getAutocrine(tt, TAMode.TAMreactLS, 30)
+        secondSurf = TAMode.runTAMinit([100.0], tt, TAMode.TAMreactLS, firstSurf)
 
-    tt.expression = 0.0
-    tt.kRec = 0.0
-    tt.internalize = 0.0
-    tt.pYinternalize = 0.0
-    tt.curL = (10, 10)
-
-    secondSurf = TAMode.runTAMinit([100.0], tt, TAMode.TAMreactLS, firstSurf)
-
-    reduce = TAMode.surfaceLS .* TAMode.totalLS
-    @test isapprox(dot(firstSurf, reduce), dot(secondSurf, reduce), rtol = 1.0e-5)
+        reduce = TAMode.surfaceLS .* TAMode.totalLS
+        @test isapprox(dot(firstSurf, reduce), dot(secondSurf, reduce), rtol = 1.0e-5)
+    end
 end
