3916602584d575205e84d1811650a4c105973d21
Aaron Meyer <a@ameyer.me>
2020-03-27 14:03:19 -0700

    All set.

--- src/compModel.jl
+++ src/compModel.jl
@@ -5,26 +5,46 @@ const fraction = 0.1 # Fraction of the cell surface with PS
 function compParamm(params::Vector{T})::comprates{T} where {T}
     @assert all(params .>= 0.0)
 
-    TAM, hetRs = paramTAMrate(view(params, 4:11))
+    TAM, hetRs = paramTAMrate(view(params, 5:12))
 
-    out = comprates{T}(TAM, params[1], params[2], params[3], hetRs)
+    out = comprates{T}(TAM, params[1], params[2], params[3], params[4], hetRs)
 
     return detailedBalance(out)
 end
 
 
-function TAMreact(dxxdt_d::Vector, xx_d::Vector, p::comprates, t)
-    # Note that TAM_reacti will set ydot to zero first
-    # Reaction for the rich phase
-    p.rr.gasCur *= p.gasPart
-    TAM_reacti(dxxdt_d, xx_d, p.rr, t)
+function TAMreact(du::Vector, u::Vector, r::comprates, t)
+    fill!(du, 0.0)
+    cache = Vector{promote_type(eltype(du), typeof(r.gasCur))}(undef, 10)
 
-    # Reaction for the poor phase
-    p.rr.gasCur /= p.gasPart
-    TAM_reacti(dxxdt_d[(Nspecies + 1):end], xx_d[(Nspecies + 1):end], p.rr, t)
+    compartmentReact(u, du, r.gasCur, nothing, r, cache)
+end
 
-    # Deal with transport across boundary
-    transIn = p.partIn * p.fraction
-    dxxdt_d[1:Nspecies] += (transIn * xx_d[(Nspecies + 1):(2 * Nspecies)] - xx_d[(Nspecies + 1):(2 * Nspecies)] .* boundLig) / p.fraction
-    dxxdt_d[(Nspecies + 1):(2 * Nspecies)] -= transIn * (xx_d[(Nspecies + 1):(2 * Nspecies)] - xx_d[1:Nspecies] .* boundLig) / (1 - p.fraction)
+
+function TAMreactComp(du::Vector, u::Vector, r::comprates, t)
+    fill!(du, 0.0)
+    cache = Vector{promote_type(eltype(du), typeof(r.xFwd))}(undef, 10)
+
+    sizze = Int(length(u) / 27)
+    boundary = Int(floor(sizze / 10))
+
+    for ii in 0:boundary
+        compartmentReact(view(u, (27*ii + 1):(27*ii + 27)), view(du, (27*ii + 1):(27*ii + 27)), r.gasCur * r.gasPart, nothing, r, cache)
+    end
+
+    for ii in boundary:(sizze + 1)
+        compartmentReact(view(u, (27*ii + 1):(27*ii + 27)), view(du, (27*ii + 1):(27*ii + 27)), r.gasCur, nothing, r, cache)
+    end
+    
+    dx = collect(1:sizze)
+    Δ = CenteredDifference(2, 2, dx, sizze, coeff_func=r.diff)
+
+    # TODO: Handle boundary conditions
+    for ii in 1:27
+        if boundLigC[ii] == 0
+            du[ii:27:end] += Δ * u[ii:27:end]
+        else
+            du[ii:27:end] += Δ * u[ii:27:end]
+        end
+    end
 end
