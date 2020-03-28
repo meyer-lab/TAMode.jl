225486e059b6a4a45b998d2cd39f29ee8acb0b8b
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-03-28 07:35:49 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,8 @@ using Turing
 
 tps = 10.0 .^ range(-6.0, stop = 4.0, length = 25)
 
+aboutZero = x -> isapprox(x, 0.0, rtol = 1.0e-5, atol = 1.0e-5)
+
 
 include("testreactCode.jl")
 include("testCompModel.jl")
