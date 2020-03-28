1d9e22aa890b31a030bdebefec9da0236841dc93
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-03-28 08:14:16 -0700

    Accidentally disabled the tests (#151)
    * Update runtests.jl
    
    * Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,10 @@ using Turing
 
 tps = 10.0 .^ range(-6.0, stop = 4.0, length = 25)
 
+aboutZero = x -> isapprox(x, 0.0, rtol = 1.0e-5, atol = 1.0e-5)
 
-#include("testreactCode.jl")
+
+include("testreactCode.jl")
 include("testCompModel.jl")
-#include("testLS.jl")
-#include("testSample.jl")
+include("testLS.jl")
+include("testSample.jl")
