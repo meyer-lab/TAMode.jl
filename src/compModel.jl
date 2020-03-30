be8e118d5a65a0559160a10a67f274c0ab01e415
Aaron Meyer <aaron@asmlab.org>
2020-03-30 09:01:40 -0700

    Close to working.

--- src/compModel.jl
+++ src/compModel.jl
@@ -1,4 +1,5 @@
 const fraction = 0.1 # Fraction of the cell surface with PS
+const apprx_ord = 2
 
 
 " Setup the parameters for the full TAM receptor model. "
@@ -13,54 +14,56 @@ function compParamm(params::Vector{T})::comprates{T} where {T}
 end
 
 
-function TAMreact(du::Vector, u::Vector, r::comprates, t)
-    fill!(du, 0.0)
-    cache = Vector{promote_type(eltype(du), typeof(r.gasCur))}(undef, 10)
-
-    compartmentReact(u, du, r.gasCur, nothing, r, cache)
+# TODO: Boundary condition not yet working
+function getDiffOp(dx, D)
+    Δ = CenteredDifference(apprx_ord, apprx_ord, dx, length(dx) - 1, D)
+    return Δ # * Neumann0BC(dx, apprx_ord)
 end
 
 
-function TAMreactComp(du::Vector, u::Vector, r::comprates, t; reaction=true)
+function TAMreact(du::Vector, u::Vector, r::comprates, t; reaction=true)
     fill!(du, 0.0)
+    cache = Vector{promote_type(eltype(du), typeof(r.xFwd))}(undef, 10)
 
     sizze = Int(length(u) / 27)
     boundary = Int(floor(sizze / 10))
 
     if reaction
-        cache = Vector{promote_type(eltype(du), typeof(r.xFwd))}(undef, 10)
+        for ii = 0:(sizze - 1)
+            if ii < boundary
+                gass = r.gasCur * r.gasPart
+            else
+                gass = r.gasCur
+            end
 
-        for ii = 0:boundary
-            compartmentReact(view(u, (27 * ii + 1):(27 * ii + 27)), view(du, (27 * ii + 1):(27 * ii + 27)), r.gasCur * r.gasPart, nothing, r, cache)
-        end
-
-        for ii = boundary:(sizze + 1)
-            compartmentReact(view(u, (27 * ii + 1):(27 * ii + 27)), view(du, (27 * ii + 1):(27 * ii + 27)), r.gasCur, nothing, r, cache)
+            idx = (27 * ii + 1):(27 * ii + 27)
+            compartmentReact(view(u, idx), view(du, idx), gass, nothing, r, cache)
         end
     end
 
-    dx = collect(1:sizze)
-    Δ = CenteredDifference(2, 2, dx, sizze, coeff_func = r.diff)
-    Δin = CenteredDifference(2, 2, dx[1:boundary], boundary, coeff_func = r.diff)
-    Δout = CenteredDifference(2, 2, dx[(boundary + 1):sizze], sizze - boundary, coeff_func = r.diff)
-    bc = Neumann0BC(dx, approximation_order)
-    bcin = Neumann0BC(dx[1:boundary], approximation_order)
-    bcout = Neumann0BC(dx[(boundary + 1):sizze], approximation_order)
+    # If we're not yet dealing with the PDE, solve for starting state
+    if length(du) == 27
+        return nothing
+    end
 
-    # TODO: Handle boundary flux
+    dx = 1.0 ./ collect(1:(sizze + 1))
+    bc = getDiffOp(dx, r.diff)
+    bcin = getDiffOp(dx[1:boundary], r.diff)
+    bcout = getDiffOp(dx[(boundary + 1):sizze], r.diff)
+
     for ii = 1:27
         duu = @view du[ii:27:end]
         uu = u[ii:27:end]
 
-        if boundLigC[ii] == 0
-            duu += (Δ * bc) * uu
-        else
-            duu[1:boundary] += (Δin * bcin) * uu[1:boundary]
-            duu[(boundary + 1):end] += (Δout * bcout) * uu[(boundary + 1):end]
+    #    if boundLigC[ii] == 0
+    #        duu += bc * uu
+    #    else
+    #        duu[1:boundary] += bcin * uu[1:boundary]
+    #        duu[(boundary + 1):end] += bcout * uu[(boundary + 1):end]
 
-            fluxx = (uu[boundary + 1] - uu[boundary]) / sizze / sizze * r.diff
-            du[boundary] += fluxx
-            du[boundary + 1] -= fluxx
-        end
+    #        fluxx = (uu[boundary + 1] - uu[boundary]) / sizze / sizze * r.diff
+    #        du[boundary] += fluxx
+    #        du[boundary + 1] -= fluxx
+    #    end
     end
 end
