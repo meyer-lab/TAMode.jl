fa3bb30d4ef0698333b6f56823b0c6a3b3c8971e
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-04-03 17:32:26 -0700

    Update testLS.jl

--- test/testLS.jl
+++ test/testLS.jl
@@ -54,9 +54,9 @@
     @testset "LS: Ensure that system reaches equilibrium." begin
         tt = TAMode.Lsparam(fill(0.2, 9))
 
-        uLong = TAMode.getAutocrine(tt, TAMode.TAMreactLS, 30)
+        uLong = TAMode.getAutocrine(tt)
         dnorm = zeros(30)
         TAMode.TAMreactLS(dnorm, uLong, tt, 0.0)
-        @test norm(dnorm) .< 0.01
+        @test norm(dnorm) .< 0.001
     end
 end
