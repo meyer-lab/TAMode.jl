bd6848200f582406ae89da6e508e471b45f23477
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-04-07 09:00:37 -0700

    Work-precision diagram. (#170)
    * Work-precision diagram.
    
    * Update Project.toml

---
+++ wps.jl
@@ -0,0 +1,31 @@
+using TAMode
+using Plots
+using OrdinaryDiffEq
+using DiffEqDevTools
+using IterativeSolvers
+
+params = TAMode.param(ones(15) * 0.5)
+solInit = TAMode.getAutocrine(params)
+params.gasCur = 100.0
+prob = ODEProblem(TAMode.TAMreact, solInit, (0.0, 1000.0), params)
+
+setups = [
+    Dict(:alg => Rodas4()),
+    Dict(:alg => Rodas42()),
+    Dict(:alg => Rodas4P()),
+    Dict(:alg => Ros4LStab()),
+    Dict(:alg => Rodas5()),
+    Dict(:alg => Rosenbrock23()), # slow
+    Dict(:alg => TRBDF2(autodiff=false, linsolve=LinSolveGMRES())), # slow
+    Dict(:alg => ABDF2(autodiff=false, linsolve=LinSolveGMRES())), # slow
+    # Dict(:alg => Exprb43()), # slow
+    # Dict(:alg => Exprb32()), # slow
+]
+
+test_sol = solve(prob, AutoTsit5(Rodas5()), reltol = 1.0e-16, abstol = 1.0e-16)
+
+tols = 1.0 ./ 10.0 .^ LinRange(9, 12, 4)
+
+wp = WorkPrecisionSet(prob, tols, tols, setups; save_everystep = false, appxsol = test_sol, maxiters = Int(1e6))
+
+plot(wp)
