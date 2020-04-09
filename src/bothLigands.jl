245e506ba1c42d8016f39be52fa165f1d9d63805
ajtsao1 <58233382+ajtsao1@users.noreply.github.com>
2020-04-09 14:12:59 -0700

    LS ligand vectors (#174)
    * ligand vectors
    
    * Update bothLigands.jl
    
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- src/bothLigands.jl
+++ src/bothLigands.jl
@@ -104,6 +104,8 @@ end
 totalLS = vcat(ones(9), 2 * ones(5), ones(9) * internalFrac, 2 * ones(5) * internalFrac, zeros(2))
 surfaceLS = vcat(ones(14), zeros(16))
 pYcLS = vcat(zeros(9), ones(5))
+GascLS = vcat(0, ones(2), zeros(2), 2, 1, 0, 1, 2, 1, zeros(2), 1)
+PROScLS = vcat(zeros(3), ones(2), 0, 1, 2, 1, zeros(2), 1, 2, 1)
 
 
 function TAMreact(dR, R, tr::Lsrates, t)
