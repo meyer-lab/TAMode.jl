2f0a17bd64c8918ce3b60d6c72601f3a37244c76
ajtsao1 <58233382+ajtsao1@users.noreply.github.com>
2020-04-14 09:42:16 -0700

    LS vectors and tests (#182)
    * test no ligand
    
    * LS vectors and tests
    
    * Update testLS.jl
    
    * Update testLS.jl
    
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- test/testLS.jl
+++ test/testLS.jl
@@ -59,4 +59,31 @@
         TAMode.TAMreact(dnorm, uLong, tt, 0.0)
         @test norm(dnorm) .< 0.001
     end
+    
+    @testset "LS: Test that if no Gas6 is present, we don’t see any." begin
+        rr = TAMode.Lsparam(fill(0.2, 9))
+        rr.curL = (0.0, 0.0)
+
+        data = TAMode.runTAM(tps, rr, (0.0, 10.0))
+
+        @test all(aboutZero.(data * TAMode.GasLS))
+    end
+    
+    @testset "LS: Test that if no Protein S is present, we don’t see any." begin
+        rr = TAMode.Lsparam(fill(0.2, 9))
+        rr.curL = (0.0, 0.0)
+
+        data = TAMode.runTAM(tps, rr, (10.0, 0.0))
+
+        @test all(aboutZero.(data * TAMode.PROSLS))
+    end
+    
+    @testset "LS: Test that if no ligand is present, we don’t see any pY." begin
+        rr = TAMode.Lsparam(fill(0.2, 9))
+        rr.curL = (0.0, 0.0)
+
+        data = TAMode.runTAM(tps, rr, (0.0, 0.0))
+
+        @test all(aboutZero.(data * TAMode.pYLS))
+    end
 end
