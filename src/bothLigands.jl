cf9744a1fe7bd3cd8eff80baf51dd2fc822b90c3
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-04-16 18:05:36 -0700

    Update bothLigands.jl

--- src/bothLigands.jl
+++ src/bothLigands.jl
@@ -99,7 +99,6 @@ end
 totalLS = vcat(ones(9), 2 * ones(5), ones(9) * internalFrac, 2 * ones(5) * internalFrac, zeros(2))
 surfaceLS = vcat(ones(14), zeros(16))
 pYcLS = vcat(zeros(9), ones(5))
-pYLS = vcat(pYcLS, pYcLS, zeros(2))
 GascLS = vcat(0, ones(2), zeros(2), 2, 1, 0, 1, 2, 1, zeros(2), 1)
 PROScLS = vcat(zeros(3), ones(2), 0, 1, 2, 1, zeros(2), 1, 2, 1)
 pYLS = vcat(pYcLS, pYcLS, zeros(2))
