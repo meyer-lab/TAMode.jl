68295195b09e9e97184c7c7739c4e3ee3bcf7146
sumedha-k <sumedha.kanthamneni@gmail.com>
2020-10-08 16:43:03 -0700

    added sampling file

---
+++ sampling.jl
@@ -0,0 +1,14 @@
+import TAMode
+using Test
+using Turing
+
+f = open("log.txt", "w")
+redirect_stdout(f)
+redirect_stderr(f)
+
+@testset  "Test sampling." begin
+    samp = sample(TAMode.A549model, NUTS(0.65), 500)
+    write("chain-file-10_1.jls", samp)
+end
+
+close(f)
